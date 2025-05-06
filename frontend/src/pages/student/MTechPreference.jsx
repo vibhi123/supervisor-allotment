917f2efdf5ec8041e10ce57b0c72e693634f0602
srvmandal24 <srvmandal24@gmail.com>
2025-05-06 22:40:50 +0530

    Bug fixed

--- frontend/src/pages/student/MTechPreference.jsx
+++ frontend/src/pages/student/MTechPreference.jsx
@@ -3,10 +3,12 @@ import axios from 'axios';
 import FacultyPreferenceSelector from '../../components/FacultyPreferenceSelector';
 import { Container, CircularProgress, Alert } from '@mui/material';
 import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 const MTechPreference = () => {
   const [faculties, setFaculties] = useState([]);
   const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get('http://localhost:8000/api/v1/faculty/all-faculty')
@@ -26,11 +28,14 @@ const MTechPreference = () => {
   const handlePreferenceSubmit = async (preferenceIds) => {
         // console.log(preferenceIds);
         try {
-          await axios.post('http://localhost:8000/api/v1/student/submit-preferences',
+          const res = await axios.post('http://localhost:8000/api/v1/student/submit-preferences',
             {preferenceIds: preferenceIds},
           {
             withCredentials: true
           });
+          
+          toast.success("Preferences submitted successfully");
+          navigate(`/student/${res.data.data.registrationNumber}`);
         } catch (error) {
           console.error(error);
         }
